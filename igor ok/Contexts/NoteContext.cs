dd7d8a2640fc0e3fb5624eb9b99be4b0f07ad4db
StudentDev <Fortnite71rusia@gmail.com>
2021-10-02 17:55:56 +0300

    Мега хайповый первый урок

---
+++ igor ok/Contexts/NoteContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using igor_ok.Models;
+
+namespace igor_ok.Contexts
+{
+    public class NoteContext : DbContext
+    {
+        public NoteContext(DbContextOptions<NoteContext> option) : base(option)
+        {
+
+        }
+
+        public DbSet<Note> Notes { get; set; }
+
+    }
+}
